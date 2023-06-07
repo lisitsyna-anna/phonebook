fc3bc65b25e473cad5905ea65eddd74f9b9e6170
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2023-06-07 15:36:46 +0300

    fix fonts

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { store, persistor } from './redux/store';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
